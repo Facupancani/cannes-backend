1e5adffd74508d9bcc961e473f98b40b15d94374
Facundo Pancani <facupancani@gmail.com>
2024-10-31 16:54:30 -0300

    agrega un put para actualizar estado de prendas en lavadero

--- src/controllers/LaundryController.js
+++ src/controllers/LaundryController.js
@@ -12,3 +12,39 @@ exports.getItemsInLaundry = async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 };
+
+// Envia prendas al lavadero
+exports.sendItemsToLaundry = async (req, res) => {
+    try {
+        const items = req.body.items; // Asegúrate de que los datos enviados tienen el arreglo bajo la clave 'items'
+
+        // Validación para asegurar que items esté definido y sea un arreglo
+        if (!Array.isArray(items)) {
+            return res.status(400).json({ error: 'El formato de datos es incorrecto. Se esperaba un arreglo de items.' });
+        }
+
+        // Procesa cada prenda en el arreglo
+        const results = await Promise.all(items.map(async (item) => {
+            const { article_id, amount, name } = item;
+
+            // Verifica si ya existe una entrada para el artículo en el depósito 4
+            const existingItem = await Laundry.findOne({
+                where: { article_id, deposit: 4 },
+            });
+
+            if (existingItem) {
+                // Si ya existe, actualiza la cantidad sumando el nuevo envío
+                existingItem.amount += amount;
+                await existingItem.save();
+                return { article_id, name, status: 'actualizado', amount: existingItem.amount };
+            } else {
+                return res.status(500).json({ error: 'Error al enviar prendas al lavadero, el articulo no' });
+            }
+        }));
+
+        return res.status(200).json({ message: 'Prendas enviadas al lavadero con éxito', details: results });
+    } catch (error) {
+        console.error('Error al enviar prendas al lavadero:', error);
+        return res.status(500).json({ error: 'Error al enviar prendas al lavadero' });
+    }
+};
--- src/routes/LaundryRoutes.js
+++ src/routes/LaundryRoutes.js
@@ -5,5 +5,7 @@ const LaundryController = require('../controllers/LaundryController');
 // Obtener las prendas que debe el lavadero
 router.get('/laundry/pending', LaundryController.getItemsInLaundry);
 
+// Envia prendas al lavadero
+router.put('/laundry/send', LaundryController.sendItemsToLaundry);
 
 module.exports = router;
