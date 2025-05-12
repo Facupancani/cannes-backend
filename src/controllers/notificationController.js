14644c14ffad290f9f8786775788dcef9341136b
Facupancani <facupancani@hotmail.com>
2025-05-12 12:07:52 -0300

    se crea conexion con sistema de notificaciones

---
+++ src/controllers/notificationController.js
@@ -0,0 +1,78 @@
+const sequelize = require('../config/database');
+const notification = require('../models/Notification');
+
+// Obtener todas las notificaciones
+exports.getAllNotifications = async(req, res) => {
+    try {
+        const notifications = await notification.findAll()
+        res.json(notifications)
+    } catch (err){
+        res.status(500).json({ error: err.message })
+    }
+}
+
+// Obtener notificacion por ID
+exports.getNotificationById = async(req, res) => {
+    try {
+        const id = req.params.id
+        const notification = await notification.findOne({where: {id: id}})
+
+        if (notification) {
+            res.json(notification)
+        } 
+        else {
+            res.status(404).json({error: "Notification not found"})
+        } 
+    } catch (err) {
+        res.status(500).json({error: err.message})
+    }
+}
+
+// Crear notificacion
+exports.createNotification = async(req, res) => {
+    try {
+        const { title, content } = req.body
+        const created = await notification.create({ title, content });
+        const newNotification = await notification.findByPk(created.id);
+        
+        res.status(201).json(newNotification)
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+}
+
+// Eliminar notificacion por ID
+exports.deleteNotification = async(req, res) => {
+    try {
+        const id = req.params.id
+        const notification = await notification.destroy({where: {id: id}})
+
+        if (notification) {
+            res.status(204).send()
+        } 
+        else {
+            res.status(404).json({error: "Notification not found"})
+        } 
+    } catch (err) {
+        res.status(500).json({error: err.message})
+    }
+}
+
+// Actualizar notificacion por ID
+exports.updateNotification = async(req, res) => {
+    try {
+        const id = req.params.id
+        const { title, content } = req.body
+        const notification = await notification.update({title, content}, {where: {id: id}})
+
+        if (notification) {
+            res.status(204).send()
+        } 
+        else {
+            res.status(404).json({error: "Notification not found"})
+        } 
+    } catch (err) {
+        res.status(500).json({error: err.message})
+    }
+}
+
